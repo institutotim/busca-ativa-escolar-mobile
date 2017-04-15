6fc9d415947513f49875a71679ed2a14dfebbd4e
Aryel Tupinambá <aryel.tupinamba@lqdi.net>
2017-04-15 18:12:34 -0300

    Updated release script to build iOS and Android separetely

--- build-all-for-release.sh
+++ build-all-for-release.sh
@@ -1,12 +1,32 @@
 #!/bin/sh
-ionic build --device --release
+
+echo "[ LQDI-BUILDBOT ] Scaffolding environment...";
+mv src/env_api_root.ts src/env_api_root_devel.ts
+mv src/env_api_root_release.ts src/env_api_root.ts
+
+echo "[ LQDI-BUILDBOT ] Cleaning Cordova builds...";
+cordova clean
+
+echo "[ LQDI-BUILDBOT ] Building IOS...";
+rm ./release/ios-release-ready.ipa
+ionic build ios --device --release
+cp ./platforms/ios/build/device/Busca\ Ativa\ Escolar.ipa ./release/ios-release-ready.ipa
+
+echo "iOS Package generation done! Uploading iOS IPA to Itunes Connect..."
+altool --upload-app -f ./platforms/ios/build/device/Busca\ Ativa\ Escolar.ipa -u buildbot@lqdi.net -p p8HDjI4jTWw5
+
+echo "[ LQDI-BUILDBOT ] Building ANDROID...";
 rm ./release/android-release-pending.apk
 rm ./release/android-release-ready.apk
-rm ./release/ios-release-ready.ipa
-cp ./platforms/ios/build/device/Busca\ Ativa\ Escolar.ipa ./release/ios-release-ready.ipa
+ionic build android --device --release
+
+echo "[ LQDI-BUILDBOT ] Signing APK for ANDROID...";
 cp ./platforms/android/build/outputs/apk/android-release-unsigned.apk ./release/android-release-pending.apk
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -tsa http://tsa.safecreative.org -keystore ~/Dropbox/SSH/google_play_keystore.jks ./release/android-release-pending.apk busca-ativa-escolar_2017
 zipalign -v 4 ./release/android-release-pending.apk ./release/android-release-ready.apk
-echo "Package generation done! Uploading iOS IPA to Itunes Connect..."
-altool --upload-app -f ./platforms/ios/build/device/Busca\ Ativa\ Escolar.ipa -u buildbot@lqdi.net -p p8HDjI4jTWw5
-echo "Done! Now you must upload the APK to Play Store and update the current used build for TestFlight/GA in Itunes Connect."
+
+echo "[ LQDI-BUILDBOT ] De-scaffolding environment...";
+mv src/env_api_root.ts src/env_api_root_release.ts
+mv src/env_api_root_devel.ts src/env_api_root.ts
+
+echo "[ LQDI-BUILDBOT ] Done! Now you must upload the APK to Play Store and update the current used build for TestFlight/GA in Itunes Connect."
---
+++ src/env_api_root_release.ts
@@ -0,0 +1,2 @@
+const API_ROOT = 'http://api.busca-ativa-escolar.dev.lqdi.net/';
+export default API_ROOT;
